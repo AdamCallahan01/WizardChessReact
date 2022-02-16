b20a6bee9f67bbaa33faa1e1444f015fd7013052
AdamCallahan01 <adamcallahan2001@gmail.com>
2022-02-15 18:53:28 -0800

    update readME

--- src/Components/pages/DesignIdea.js
+++ src/Components/pages/DesignIdea.js
@@ -8,14 +8,15 @@ function DesignIdea() {
     <>
       <div className='hero-container'>
       <h1>Game Design</h1>
-      <p>Project Goals
-1.	Create a chess interface
-2.	Implement different classes/formations
-3.	Implement items/abilities
-4.	Implement the ability to combine pieces/ make new pieces
-5.	Make the game playable over the website
-6.	Create new ways to win 
-</p>
+      <ul>Project Goals:
+        <li>1.	Create a chess interface</li>
+        <li>2.	Implement different classes/formations</li>
+        <li>3.	Implement items/abilities</li>
+        <li>4.	Implement the ability to combine pieces/ make new pieces</li>
+        <li>5.	Make the game playable over the website</li>
+        <li>6.	Create new ways to win</li>
+      </ul>
+      <img src="./public/images/CHess.jpg"></img>
     </div>
     </>
   );
