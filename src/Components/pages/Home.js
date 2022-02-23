a5312840b2e930636f7817e70b6327d915d1060a
AdamCallahan01 <adamcallahan2001@gmail.com>
2022-02-22 21:12:53 -0800

    Added Chessboard w/ Random moves

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -3,11 +3,13 @@ import '../../App.css';
 import Cards from '../Cards';
 import DefaultPage from '../DefaultPage';
 import Footer from '../Footer';
+import Chessboard from '../Chessboard'
 
 function Home() {
   return (
     <>
       <DefaultPage />
+      <Chessboard/>
       <Footer />
     </>
   );
